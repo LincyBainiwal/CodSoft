e54b5e6288a8ebc524bcfe652ed150b3ae91fe27
LincyBainiwal <ui22ec39@iiitsurat.ac.in>
2024-07-21 17:11:26 +0530

    Frontend and Backend

--- TaskManagementApp/frontend/src/redux/slices/authSlice.js
+++ TaskManagementApp/frontend/src/redux/slices/authSlice.js
@@ -1,5 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
-
+import {user} from "../../assets/data"
 const initialState = {
     user: localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
